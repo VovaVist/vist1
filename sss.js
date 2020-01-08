0a658dba979a3d05543a6804cfc45a432f94cd19
Ivan <doter133720@gmail.com>
2020-01-08 23:24:19 +0400

    Это наш первый коммит

---
+++ sss.js
@@ -0,0 +1,25 @@
+'use strict';
+// alert(Sup);
+// let answer = confirm('Delete the system?');
+// console.log(answer); 
+
+// let m = ["aaa.png", "bbb.png"];
+
+// let answer = prompt('Ваше первое домашнее животное', "A cat");
+// console.log(answer); 
+
+//  console.log("axy" + "et");
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5 % 2);
+console.log("2" == 2);
+
+let isChecked = false,
+    isClose = true;
+
+console.log(!isChecked && isClose);   
